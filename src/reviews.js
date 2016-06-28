b921b8b354e744ba18ebfd0966fa510fc9a937fe
Pavel <sergeevps@gmail.com>
2016-06-29 00:47:32 +0500

    Добавил обработчики неудачной загрузки xhr

--- src/reviews.js
+++ src/reviews.js
@@ -4,6 +4,7 @@ var reviewsList = document.querySelector('.reviews-list');
 var templateElement = document.querySelector('template');
 var elementToClone;
 var IMAGE_LOAD_TIMEOUT = 10000;
+var XHR_LOAD_TIMEOUT = 15000;
 var REVIEW_LOAD_URL = '//o0.github.io/assets/json/reviews.json';
 var ratings = [
   'review-rating-two',
@@ -60,15 +61,31 @@ var renderReviews = function(reviews) {
 
 var getReviews = function(callback) {
   var xhr = new XMLHttpRequest();
+  var reviewsSection = document.querySelector('.reviews');
+  var xhrTimeout;
 
   xhr.onload = function(evt) {
     var loadedData = JSON.parse(evt.target.response);
+    clearTimeout(xhrTimeout);
+    reviewsSection.classList.remove('reviews-load-failure');
+    reviewsSection.classList.remove('reviews-list-loading');
     callback(loadedData);
   };
-  
+
   xhr.onloadstart = function() {
-    
+    reviewsSection.classList.add('reviews-list-loading');
   };
+
+  xhr.onerror = function() {
+    clearTimeout(xhrTimeout);
+    reviewsSection.classList.remove('reviews-list-loading');
+    reviewsSection.classList.add('reviews-load-failure');
+  };
+
+  xhrTimeout = setTimeout(function() {
+    reviewsSection.classList.remove('reviews-list-loading');
+    reviewsSection.classList.add('reviews-load-failure');
+  }, XHR_LOAD_TIMEOUT);
 
   xhr.open('GET', REVIEW_LOAD_URL);
   xhr.send();
